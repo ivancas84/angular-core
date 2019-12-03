7f5c504532ba786d4b7d8494cf17bd5876f89415
ivan <ivancas84@gmail.com>
2019-12-03 14:45:21 -0300

    corregir error al definir dates

--- service/parser/parser.service.ts
+++ service/parser/parser.service.ts
@@ -20,12 +20,12 @@ export class ParserService {
       case null: return null;
 
       case "CURRENT_TIME": case "CURRENT_DATE": case "CURRENT_TIMESTAMP":
-        let date: Date = new Date();
+        var date: Date = new Date();
         //return date;
         return new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000); //sumamos offset correspondiente a la hora local (offset_minutos * segundos * transformar_a_milisegundos)
 
       default:
-        date = new Date(value);
+        var date = new Date(value);
         //return date;
         return new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000); //sumamos offset correspondiente a la hora local (offset_minutos * segundos * transformar_a_milisegundos)
     }
