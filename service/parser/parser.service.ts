30cfb3ffcc4afdd14d2cc0d0f0ca1977db49e6b2
unknown <ivancas84@gmail.com>
2020-03-09 06:20:55 -0300

    Nuevo pipe toTime. Nuevo parser time

--- service/parser/parser.service.ts
+++ service/parser/parser.service.ts
@@ -22,12 +22,36 @@ export class ParserService {
       case "CURRENT_TIME": case "CURRENT_DATE": case "CURRENT_TIMESTAMP":
         var date: Date = new Date();
         //return date;
-        return new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000); //sumamos offset correspondiente a la hora local (offset_minutos * segundos * transformar_a_milisegundos)
+        return new Date(date.getTime())
+        // + date.getTimezoneOffset() * 60 * 1000); //sumamos offset correspondiente a la hora local (offset_minutos * segundos * transformar_a_milisegundos)
 
       default:
         var date = new Date(value);
         //return date;
-        return new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000); //sumamos offset correspondiente a la hora local (offset_minutos * segundos * transformar_a_milisegundos)
+        return new Date(date.getTime())
+        // + date.getTimezoneOffset() * 60 * 1000); //sumamos offset correspondiente a la hora local (offset_minutos * segundos * transformar_a_milisegundos)
+    }
+  }
+
+  time(value: string): Date {
+    if(!value) return null;
+
+    switch(value){
+      case null: return null;
+
+      case "CURRENT_TIME": case "CURRENT_DATE": case "CURRENT_TIMESTAMP":
+        var date: Date = new Date();
+        //return date;
+        return new Date(date.getTime())
+        // + date.getTimezoneOffset() * 60 * 1000); //sumamos offset correspondiente a la hora local (offset_minutos * segundos * transformar_a_milisegundos)
+
+      default:
+        var dateAux = new Date();
+        var date = new Date(this.dateFormat(dateAux) + " " + value);
+
+        //return date;
+        return new Date(date.getTime())
+        // + date.getTimezoneOffset() * 60 * 1000); //sumamos offset correspondiente a la hora local (offset_minutos * segundos * transformar_a_milisegundos)
     }
   }
 
