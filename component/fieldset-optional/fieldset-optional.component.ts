22819ad273980bb1cee4c7641ee5530674d719dd
ivancas84 <ivancas84@gmail.com>
2020-04-01 20:51:32 -0300

    corregir errores de compilacion

--- component/fieldset-optional/fieldset-optional.component.ts
+++ component/fieldset-optional/fieldset-optional.component.ts
@@ -1,7 +1,25 @@
 import { FieldsetComponent } from '@component/fieldset/fieldset.component';
+import { Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 export abstract class FieldsetOptionalComponent extends FieldsetComponent {
 
+  @Input() data$: any; 
+  /**
+   * Deberia heredarlo pero da error al ejecutar "ng s"
+   * Se ve que solo resuelve hasta una superclase
+   * @todo Conviene reemplazar el uso de FieldsetComponent y reimplementar todo en FieldsetOptional
+   * Datos del formulario
+   */
+
+  @Input() form: FormGroup; 
+  /**
+   * Deberia heredarlo pero da error al ejecutar "ng s"
+   * Se ve que solo resuelve hasta una superclase
+   * @todo Conviene reemplazar el uso de FieldsetComponent y reimplementar todo en FieldsetOptional
+   * Formulario de administracion
+   */
+  
   initData(): void{    
     this.data$.subscribe(
       response => {
